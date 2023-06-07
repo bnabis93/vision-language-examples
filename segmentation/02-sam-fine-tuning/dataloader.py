0ba677acfe515de430e7757fa713108b89e3ea4c
Hyeonwoo Jeong <hyeonwoo-jeong@a1>
2023-06-07 08:12:05 +0000

    feat: fine-tuning example

--- segmentation/02-sam-fine-tuning/dataloader.py
+++ segmentation/02-sam-fine-tuning/dataloader.py
@@ -1,10 +1,58 @@
+import os
+import numpy as np
+import torch
 from torch.utils.data import Dataset
+
+
+join = os.path.join
+
+
 class NpzDataset(Dataset):
     """create a dataset class to load npz data and return back image embeddings and ground truth"""
-    def __init__(self, data_path : str):
-        self_data_path = data_path
-        return 
+
+    def __init__(self, data_path: str):
+        self.data_path = data_path
+        self.npz_files = sorted(os.listdir(self.data_path))
+        self.npz_data = [np.load(join(data_path, f)) for f in self.npz_files]
+
+        # this implementation is ugly but it works (and is also fast for feeding data to GPU) if your server has enough RAM
+        # as an alternative, you can also use a list of npy files and load them one by one
+        self.ori_gts = np.vstack([data["gts"] for data in self.npz_data])
+        self.img_embeddings = np.vstack(
+            [data["img_embeddings"] for data in self.npz_data]
+        )
+        print(f"{self.img_embeddings.shape=}, {self.ori_gts.shape=}")
+
     def __len__(self):
-        return
-    def __getitem__(self, idx):
-        return 
+        return self.ori_gts.shape[0]
+
+    def __getitem__(self, index):
+        img_embed = self.img_embeddings[index]
+        gt2D = self.ori_gts[index]
+        y_indices, x_indices = np.where(gt2D > 0)
+        x_min, x_max = np.min(x_indices), np.max(x_indices)
+        y_min, y_max = np.min(y_indices), np.max(y_indices)
+        # add perturbation to bounding box coordinates
+        H, W = gt2D.shape
+        x_min = max(0, x_min - np.random.randint(0, 20))
+        x_max = min(W, x_max + np.random.randint(0, 20))
+        y_min = max(0, y_min - np.random.randint(0, 20))
+        y_max = min(H, y_max + np.random.randint(0, 20))
+        bboxes = np.array([x_min, y_min, x_max, y_max])
+        xy_center_points = np.array(get_center_points(bboxes))
+
+        # convert img embedding, mask, bounding box, center point to torch tensor
+        return (
+            torch.tensor(img_embed).float(),
+            torch.tensor(gt2D[None, :, :]).long(),
+            torch.tensor(bboxes).float(),
+            torch.tensor(xy_center_points).float(),
+        )
+
+
+def get_center_points(bbox: np.ndarray):
+    """get the center points(xy format) of the bounding box"""
+    x_min, y_min, x_max, y_max = bbox
+    x_center = (x_min + x_max) / 2
+    y_center = (y_min + y_max) / 2
+    return x_center, y_center
--- segmentation/02-sam-fine-tuning/fine_tuning.py
+++ segmentation/02-sam-fine-tuning/fine_tuning.py
@@ -1,6 +1,94 @@
+import os
 import torch
 import numpy as np
+import monai
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from dataloader import NpzDataset
+from torch.utils.data import DataLoader
+from segment_anything import sam_model_registry
+from segment_anything.utils.transforms import ResizeLongestSide
+
+join = os.path.join
+
+# Set global variable
+## [TODO]: Will be managed by config file
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+npz_dataset_path = "data/demo2D_vit_b"
+data_batch = 4
+model_type = "vit_b"
+checkpoint = "models/sam_vit_b_01ec64.pth"
+save_path = "./outputs"
+num_epochs = 100
 
 # Set seeds
 torch.manual_seed(2023)
 np.random.seed(2023)
+
+# Load the dataset
+dataset = NpzDataset(npz_dataset_path)
+dataloader = DataLoader(dataset, batch_size=data_batch, shuffle=True)
+
+# Load the model and model parameters
+model = sam_model_registry[model_type](checkpoint=checkpoint).to(device)
+model.train()
+optimizer = torch.optim.Adam(model.mask_decoder.parameters(), lr=1e-5, weight_decay=0)
+seg_loss = monai.losses.DiceCELoss(sigmoid=True, squared_pred=True, reduction="mean")
+
+# Train the model
+losses = []
+best_loss = 1e10
+for epoch in range(num_epochs):
+    epoch_loss = 0
+    ## img_embed: (B, 256, 64, 64), gt2D: (B, 1, 256, 256), bboxes: (B, 4), points: (B, 2), B= batch size
+    for step, (image_embedding, gt2D, boxes, points) in enumerate(tqdm(dataloader)):
+        with torch.no_grad():  # Encoder part, no need to calculate gradients.
+            box_np = boxes.numpy()
+            sam_trans = ResizeLongestSide(model.image_encoder.img_size)
+
+            # Training the model w/ bbox
+            box = sam_trans.apply_boxes(box_np, (gt2D.shape[-2], gt2D.shape[-1]))
+            box_torch = torch.as_tensor(box, dtype=torch.float, device=device)
+            if len(box_torch.shape) == 2:
+                box_torch = box_torch[:, None, :]  # (B, 1, 4)
+
+            # get prompt embeddings
+            sparse_embeddings, dense_embeddings = model.prompt_encoder(
+                points=None,
+                boxes=box_torch,
+                masks=None,
+            )
+
+        # Decoder part, calculate gradients.
+        mask_predictions, _ = model.mask_decoder(
+            image_embeddings=image_embedding.to(device),  # (B, 256, 64, 64)
+            image_pe=model.prompt_encoder.get_dense_pe(),  # (1, 256, 64, 64)
+            sparse_prompt_embeddings=sparse_embeddings,  # (B, 2, 256)
+            dense_prompt_embeddings=dense_embeddings,  # (B, 256, 64, 64)
+            multimask_output=False,
+        )
+
+        loss = seg_loss(mask_predictions, gt2D.to(device))
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+
+    epoch_loss /= step
+    losses.append(epoch_loss)
+
+    print(f"EPOCH: {epoch}, Loss: {epoch_loss}")
+    # save the latest model checkpoint
+    torch.save(model.state_dict(), join(save_path, "sam_model_latest.pth"))
+    # save the best model
+    if epoch_loss < best_loss:
+        best_loss = epoch_loss
+        torch.save(model.state_dict(), join(save_path, "sam_model_best.pth"))
+
+# plot loss
+plt.plot(losses)
+plt.title("Dice + Cross Entropy Loss")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.savefig(join(save_path, "train_loss.png"))
+plt.close()
