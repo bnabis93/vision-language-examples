2d6009fc17e0c2d403424d56785f6f6aacf11d84
bono <qhsh9713@gmail.com>
2023-08-04 01:22:18 +0900

    delete: batch

--- acceleration/99-decoder-test/benchmark.py
+++ acceleration/99-decoder-test/benchmark.py
@@ -19,26 +19,19 @@ x1 = Variable(torch.randn(1, 64, 256, 256)).to(device)
 x2 = Variable(torch.randn(1, 128, 128, 128)).to(device)
 x3 = Variable(torch.randn(1, 256, 64, 64)).to(device)
 x4 = Variable(torch.randn(1, 512, 32, 32)).to(device)
+# Warm up
+for _ in range(10):
+    decoder(x4, x3, x2, x1)
 
-# Define input
-batch_sizes = [1, 2, 4, 8, 16, 32, 64]
-for batch in batch_sizes:
-
-    # Warm up
-    for _ in range(10):
+# Inference
+inference_times = []
+with torch.no_grad():
+    for _ in range(100):
+        torch.cuda.synchronize()
+        start = time.time()
         decoder(x4, x3, x2, x1)
+        torch.cuda.synchronize()
+        end = time.time()
+        inference_times.append((end - start) * 1000)
 
-    # Inference
-    inference_times = []
-    with torch.no_grad():
-        for _ in range(100):
-            torch.cuda.synchronize()
-            start = time.time()
-            decoder(x4, x3, x2, x1)
-            torch.cuda.synchronize()
-            end = time.time()
-            inference_times.append((end - start) * 1000)
-
-    print(
-        f"batch size :{batch}\nViT average inference time : {sum(inference_times)/len(inference_times)}ms"
-    )
+print(f"decoder average inference time : {sum(inference_times)/len(inference_times)}ms")
