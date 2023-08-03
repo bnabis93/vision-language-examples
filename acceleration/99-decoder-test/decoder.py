0c1bedbf0cfa8d0ff3570b3d821f2bc582a11672
bono <qhsh9713@gmail.com>
2023-08-04 01:48:25 +0900

    fix: transpose2D to upsample

--- acceleration/99-decoder-test/decoder.py
+++ acceleration/99-decoder-test/decoder.py
@@ -5,14 +5,26 @@ class Decoder(nn.Module):
     def __init__(self, num_classes):
         super(Decoder, self).__init__()
 
-        self.transconv1 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.transconv2 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.transconv3 = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
-        self.transconv4 = nn.ConvTranspose2d(32, num_classes, kernel_size=2, stride=2)
+        self.upsample1 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True),
+            nn.Conv2d(256, 128, kernel_size=1),
+        )
+        self.upsample2 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True),
+            nn.Conv2d(128, 64, kernel_size=1),
+        )
+        self.upsample3 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True),
+            nn.Conv2d(64, 32, kernel_size=1),
+        )
+        self.upsample4 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True),
+            nn.Conv2d(32, num_classes, kernel_size=1),
+        )
 
     def forward(self, x):
-        x = self.transconv1(x)
-        x = self.transconv2(x)
-        x = self.transconv3(x)
-        x = self.transconv4(x)
+        x = self.upsample1(x)
+        x = self.upsample2(x)
+        x = self.upsample3(x)
+        x = self.upsample4(x)
         return x
